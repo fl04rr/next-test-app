21f2641a5444313897d5e324b092fea2e0e4ed37
Vladislav Datsuk <vladdskils@gmail.com>
2023-10-30 14:41:26 +0300

    add sass

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from './layout.module.css';
-import utilStyles from '../styles/util.module.css';
+import utilStyles from '../styles/util.module.scss';
 import Link from 'next/link';
 import { gsap } from "gsap/dist/gsap";
 import { useLayoutEffect, useRef } from 'react'
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/util.module.css';
+import utilStyles from '../styles/util.module.scss';
 import { getSortedPostsData } from '../lib/posts';
 
 export async function getStaticProps(){
